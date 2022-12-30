8ea981aa7a80c61deeebc6f88dff387b7cc9482e
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2022-12-30 08:46:45 +0530

    soln xor-operation-in-an-array

---
+++ lc1486.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/xor-operation-in-an-array/
+public class lc1486 {
+    class Solution {
+    public int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            nums[i] = start + 2 * i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
+}
